ad120548bfe795ad56d3ef2ce90df55b12ae3e6c
RENZOje <RENZOje@users.noreply.github.com>
2020-07-14 14:54:08 +0300

    Update admin

--- movies/admin.py
+++ movies/admin.py
@@ -3,12 +3,70 @@ from .models import Category, Actor, Genre, Movie, MovieShots, Rating, RatingSta
 
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name','url')
+    list_display_links = ('name',)
 
-admin.site.register(Category)
-admin.site.register(Actor)
-admin.site.register(Genre)
-admin.site.register(MovieShots)
-admin.site.register(Movie)
-admin.site.register(Rating)
+
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ('email','name')
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title','category','url','draft')
+    list_filter = ('category','year')
+    search_fields = ('title','category__name')
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft',)
+    fieldsets = (
+        (None, {
+            'fields':(('title','tag_line'),)
+        }),
+        (None, {
+            'fields': (('year', 'world_premier','country'),)
+        }),
+        ('Actors', {
+            'classes':('collapse',),
+            'fields': (('actors','genres','directors','category'),)
+        }),
+        (None, {
+            'fields': (('budget', 'fees_in_usa','fees_in_world'),)
+        }),
+        ('Options', {
+            'fields': (('url', 'draft'),)
+        }),
+    )
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('name','email','parent','movie','id')
+    readonly_fields = ('name','email')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name','url')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name','age',)
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('movie','ip',)
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    list_display = ('title','movie',)
+
+
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
