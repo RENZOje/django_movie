fcdf191129e1502852d1412dd412b99e76facd8d
RENZOje <RENZOje@users.noreply.github.com>
2020-07-14 18:36:55 +0300

    Add ckeditor

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Category, Actor, Genre, Movie, MovieShots, Rating, RatingStar, Reviews
+from django.utils.safestring import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget(), label='Описание')
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 # Register your models here.
@@ -15,18 +26,36 @@ class ReviewInline(admin.TabularInline):
     readonly_fields = ('email','name')
 
 
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ('get_image',)
+    list_display = ('name','age','get_image')
+
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = 'Изображение'
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title','category','url','draft')
+    list_display = ('title','category','url','draft','get_image')
+    readonly_fields = ('get_image',)
     list_filter = ('category','year')
     search_fields = ('title','category__name')
-    inlines = [ReviewInline]
+    inlines = [MovieShotsInline,ReviewInline]
     save_on_top = True
     save_as = True
     list_editable = ('draft',)
+    form = MovieAdminForm
     fieldsets = (
         (None, {
             'fields':(('title','tag_line'),)
+        }),
+        (None, {
+            'fields': ('description', ('poster','get_image'))
         }),
         (None, {
             'fields': (('year', 'world_premier','country'),)
@@ -43,6 +72,11 @@ class MovieAdmin(admin.ModelAdmin):
         }),
     )
 
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
+
+    get_image.short_description = 'Постер'
+
 @admin.register(Reviews)
 class ReviewsAdmin(admin.ModelAdmin):
     list_display = ('name','email','parent','movie','id')
@@ -56,17 +90,32 @@ class GenreAdmin(admin.ModelAdmin):
 
 @admin.register(Actor)
 class ActorAdmin(admin.ModelAdmin):
-    list_display = ('name','age',)
+    list_display = ('name','age','get_image')
+
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = 'Изображение'
+    readonly_fields = ('get_image',)
 
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ('movie','ip',)
+    list_display = ('star','ip',)
 
 
 @admin.register(MovieShots)
 class MovieShotsAdmin(admin.ModelAdmin):
-    list_display = ('title','movie',)
+    list_display = ('title','movie','get_image')
 
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
+
+    get_image.short_description = 'Изображение'
+    readonly_fields = ('get_image',)
 
 admin.site.register(RatingStar)
+
+admin.site.site_title = 'Django Movies'
+admin.site.site_header = 'Django Movies'
--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('', MoviesView.as_view()),
     path('<slug:slug>', MovieDetailView.as_view(), name='detail_view'),
-    path('review/<int:pk>', AddReview.as_view(), name='add_review')
+    path('review/<int:pk>', AddReview.as_view(), name='add_review'),
 ]
