097d62226b4c75c8470c11b0e7add96b09c4b415
RENZOje <RENZOje@users.noreply.github.com>
2020-07-14 14:18:35 +0300

    Initial commit

---
+++ movies/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', MoviesView.as_view()),
+    path('<slug:slug>', MovieDetailView.as_view(), name='detail_view'),
+    path('review/<int:pk>', AddReview.as_view(), name='add_review')
+]
---
+++ movies/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
+from .models import *
+from .forms import *
+# Create your views here.
+
+
+
+class MoviesView(ListView):
+    model = Movie
+    template_name = 'movies/movie_list.html'
+    queryset = Movie.objects.filter(draft=False)
+
+
+
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
+    template_name = 'movies/movie_detail.html'
+
+
+class AddReview(View):
+    def post(self, request, pk):
+        form = ReviewsForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+
+        if form.is_valid():
+            form = form.save(commit=False)
+
+            if request.POST.get('parent',None):
+                form.parent_id = int(request.POST.get('parent'))
+
+            form.movie = movie
+            form.save()
+
+        return redirect(movie.get_absolute_url())
